c48c126b088b2ef18cd935eaf946d508ec3e9032
Павел Попов <pavel.popov@glowbyteconsulting.com>
2021-10-21 19:55:51 +0300

    HW. Popov

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from products.views import ProductsListView
+from django.views.decorators.cache import cache_page
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='index'),
+    path('', cache_page(3600)(ProductsListView.as_view()), name='index'),
+    # path('', ProductsListView.as_view(), name='index'),
     path('<int:category_type_id>/', ProductsListView.as_view(), name='product'),
     path('page/<int:page>/', ProductsListView.as_view(), name='page'),
     path('<int:category_type_id>/page/<int:page>/', ProductsListView.as_view(), name='filter_page')
